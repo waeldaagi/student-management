742cb0a9a9fdf793ecfb280e537d8aef9458080c
waeldaagi <daagi.wael@esprit.tn>
2025-12-06 16:45:10 +0100

    Fix formatting in StudentManagementApplicationTests.java for consistency

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,13 +2,13 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;   
 
 @SpringBootTest
-@ActiveProfiles("test")  
+@ActiveProfiles("test")
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
