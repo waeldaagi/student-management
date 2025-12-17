d822c9752c3f1cd925c16cd05c7953012e5fe1f3
waeldaagi <daagi.wael@esprit.tn>
2025-12-17 16:55:23 +0100

    test

---
+++ src/main/java/tn/esprit/studentmanagement/controllers/RootController.java
@@ -0,0 +1,13 @@
+package tn.esprit.studentmanagement.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Student Management Application is running!";
+    }
+}
